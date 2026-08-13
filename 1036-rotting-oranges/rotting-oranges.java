a443daed81d26a7c5bbf613e06377689035415e4
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-08-14 01:47:11 +0530

    Time: 2 ms (89.67%) | Memory: 44.2 MB (40.10%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.java
+++ 1036-rotting-oranges/rotting-oranges.java
@@ -1,22 +1,18 @@
 class Pair{
-    int row;
-    int col;
-    int tm;
+    int r,c,t;
 
     Pair(int r, int c, int t){
-        this.row = r; this.col = c; this.tm = t;
+        this.r = r; this.c = c; this.t = t;
     }
 }
 
 class Solution {
     public int orangesRotting(int[][] grid) {
-        int r = grid.length;
-        int c = grid[0].length;
-        
+        int t = 0, fresh = 0, count = 0, ans = 0;
+        int r = grid.length, c = grid[0].length;
         int[][] vis = new int[r][c];
         Queue<Pair> q = new LinkedList<>();
 
-        int fsh = 0;
 
         for(int i = 0; i < r; i++){
             for(int j = 0; j < c; j++){
@@ -24,39 +20,32 @@ class Solution {
                     vis[i][j] = 2;
                     q.add(new Pair(i,j,0));
                 }
-                if(grid[i][j] == 1) fsh++;
+                if(grid[i][j] == 1) fresh++;
             }
         }
 
         int[] drow = {-1, 0, 1, 0};
         int[] dcol = {0, 1, 0, -1};
-        int tm = 0;
-        int cnt = 0;
 
         while(!q.isEmpty()){
-            int row = q.peek().row;
-            int col = q.peek().col;
-            int t = q.peek().tm;
-            tm = Math.max(tm, t);
+            int row = q.peek().r;
+            int col = q.peek().c;
+            t = q.peek().t;
+            ans = Math.max(ans, t);
             q.remove();
 
             for(int i = 0; i < 4; i++){
-                int rw = row + drow[i];
-                int cl = col + dcol[i];
-
-                if(rw >= 0 && cl >= 0 && rw < r && cl < c && grid[rw][cl] == 1 && vis[rw][cl] != 2){
-                    vis[rw][cl] = 2;
-                    q.add(new Pair(rw, cl, t + 1));
-                    cnt++;
+                int new_r = row + drow[i];
+                int new_c = col + dcol[i];
+                if(new_r >=0 && new_c >= 0 && new_r < r && new_c < c && grid[new_r][new_c] == 1 && vis[new_r][new_c] != 2){
+                    vis[new_r][new_c] = 2;
+                    q.add(new Pair(new_r, new_c, t+1));
+                    count++;
                 }
             }
         }
 
-
-        if(cnt != fsh) return -1;
-        return tm;
-
-
-
+        if(count == fresh) return ans;
+        return -1;
     }
 }
