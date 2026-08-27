6e9c436571591bac3d5691f7d46e95ddf2cc9a8d
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-08-27 14:35:58 +0530

    Time: 8 ms (26.60%) | Memory: 46.8 MB (96.72%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,55 @@
+class Solution {
+    int memo(int r, int c, int[][] arr, int[][] dp) {
+        if (c < 0 || c >= arr[0].length)
+            return Integer.MAX_VALUE;
+        if (r == 0)
+            return arr[0][c];
+
+        if(dp[r][c] != -1) return dp[r][c];
+
+        int u = memo(r - 1, c, arr, dp);
+        int ld = memo(r - 1, c - 1, arr, dp);
+        int rd = memo(r - 1, c + 1, arr, dp);
+
+        return dp[r][c] = arr[r][c] + Math.min(u, Math.min(ld, rd));
+    }
+
+    int tabulation(int r, int c, int[][] arr, int[][] dp){
+        for(int i = 0; i < c; i++) dp[0][i] = arr[0][i];
+
+        for(int i = 1; i < r; i++){
+            for(int j = 0; j < c; j++){
+                int u = dp[i - 1][j];
+
+                int ld = Integer.MAX_VALUE;
+                int rd = Integer.MAX_VALUE;
+                
+                if(j > 0) ld = dp[i - 1][j - 1];
+                if(j < c - 1) rd = dp[i - 1][j + 1];
+
+                dp[i][j] = arr[i][j] + Math.min(u, Math.min(ld, rd));
+            }
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for (int i = 0; i < r; i++)
+            ans = Math.min(ans, dp[r-1][i]);
+
+        return ans;
+    }
+
+    public int minFallingPathSum(int[][] matrix) {
+        int r = matrix.length;
+        int[][] dp = new int[r][r];
+        for (int[] i : dp)
+            Arrays.fill(i, -1);
+
+        int ans = Integer.MAX_VALUE;
+
+        // for (int i = 1; i <= r; i++)
+        //     ans = Math.min(ans, memo(r - 1, r - i, matrix, dp));
+        // return ans;
+
+        return tabulation(r, r, matrix, dp);
+    }
+}
