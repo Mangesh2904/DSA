9b6c79eacc4b5bd01b8b47dd1d22f617cea1f57c
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-08-29 05:26:00 +0530

    Time: 19 ms (39.36%) | Memory: 47.3 MB (17.27%) - LeetSync

--- 322-coin-change/coin-change.java
+++ 322-coin-change/coin-change.java
@@ -42,6 +42,55 @@ class Solution {
 
     }
 
+    int space(int n, int[] coins, int amt){
+        int prev[] = new int[amt + 1];
+
+        for(int i = 0; i <= amt; i++){
+            if(i % coins[0] == 0) prev[i] = i / coins[0];
+            else prev[i] = Integer.MAX_VALUE/2;
+        }
+
+        for(int i = 1; i < n; i++){
+            int[] curr = new int[amt + 1];
+
+            for(int j = 0; j <= amt; j++){
+
+                int ntk = prev[j];
+
+                int tk = Integer.MAX_VALUE/2;
+                if(coins[i] <= j) tk = 1 + curr[j - coins[i]];
+
+                curr[j] = Math.min(tk, ntk);
+            }
+
+            prev = curr;
+        }
+
+        return prev[amt];
+    }
+
+    int Space1D(int n, int coins[], int amt){
+        int curr[] = new int[amt + 1];
+        for(int i = 0;i <= amt; i++){
+            if(i % coins[0] == 0) curr[i] = i / coins[0];
+            else curr[i] = Integer.MAX_VALUE/2;
+        }
+        for(int i = 1; i < n; i++){
+            for(int j = 0; j <= amt; j++){
+
+                int ntk = curr[j];
+
+                int tk = Integer.MAX_VALUE;
+                if(coins[i] <= j) tk = 1 + curr[j - coins[i]];
+
+                curr[j] = Math.min(tk, ntk);
+            }
+        }
+        
+        return curr[amt];
+
+    }
+
     public int coinChange(int[] coins, int amount) {
         int n = coins.length;
 
@@ -50,7 +99,9 @@ class Solution {
             Arrays.fill(i, Integer.MAX_VALUE/2);
 
         // int ans = memo(n - 1, coins, amount, dp);
-        int ans = tabulation(n, coins, amount, dp);
+        // int ans = tabulation(n, coins, amount, dp);
+        // int ans = space(n, coins, amount);         
+        int ans = Space1D(n, coins, amount);         
         return ans >= Integer.MAX_VALUE / 2 ? -1 : ans;
     }
 }
