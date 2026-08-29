5b66722408cc369fd9544bf0e562ba7e43606940
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-08-29 21:58:31 +0530

    Time: 179 ms (34.26%) | Memory: 43.4 MB (54.93%) - LeetSync

--- 494-target-sum/target-sum.java
+++ 494-target-sum/target-sum.java
@@ -1,23 +1,46 @@
 class Solution {
 
-    int memo(int n, int nums[], int tar, int[][] dp){
+    int recc(int n, int nums[], int tar, int[][] dp){
         if(n == 0){
             if(nums[0] == 0 && tar == 0) return 2;
             if(nums[n] == Math.abs(tar)) return 1;
             return 0;
         }
 
-        int pos = memo(n-1, nums, tar - nums[n], dp); 
-        int neg = memo(n-1, nums, tar + nums[n], dp); 
+        int pos = recc(n-1, nums, tar - nums[n], dp); 
+        int neg = recc(n-1, nums, tar + nums[n], dp); 
 
         return pos + neg;
     }
+
+    int memo(int n, int nums[], int tar, int[][] dp){
+
+        if(n == 0){
+            if(nums[n] == 0 && tar == 0) return 2;
+            if(nums[n] == tar || tar == 0) return 1;
+            return 0;
+        }
+
+        int tk = 0;
+        int ntk = memo(n - 1, nums, tar, dp);
+
+        if(nums[n] <= tar) tk = memo(n - 1, nums, tar - nums[n], dp);
+
+        return tk + ntk;
+    }
     
+    
+
     public int findTargetSumWays(int[] nums, int target) {
+        int total = Arrays.stream(nums).sum();
+
+        if(Math.abs(target) > total || (total - Math.abs(target)) % 2 != 0) return 0;
+
         int n = nums.length;
         int[][] dp = new int[n][Math.abs(target + 1)];
 
-        return memo(n - 1, nums, target, dp);
+        // return recc(n - 1, nums, target, dp);
+        return memo(n - 1, nums,  (total - Math.abs(target))/2, dp);
         
     }
 }
