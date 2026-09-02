1ba466732781eb3fb576a7345328a02d2173b219
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-03 00:39:00 +0530

    Time: 36 ms (11.92%) | Memory: 46.6 MB (99.17%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.java
+++ 45-jump-game-ii/jump-game-ii.java
@@ -1,18 +1,43 @@
 class Solution {
-    public int jump(int[] nums) {
-        
-        int l = 0, r = 0, n = nums.length, j = 0;
-        
-        while(r < n - 1){
-            int f = 0;
 
-            for(int i = l; i <= r; i++){
-                f = Math.max(f, i + nums[i]);
+    int memo(int n, int[] nums, int jum, int[] dp){
+        
+        if(n >= nums.length - 1) return jum;
+
+        if(dp[n] != 0) return dp[n];
+        int min = Integer.MAX_VALUE;
+
+        for(int i = 1; i <= nums[n]; i++)
+            min = Math.min(min, memo(n + i, nums, jum + 1, dp));
+
+        return dp[n] = min;
+    }
+
+    int tabu(int n, int[] nums, int jum, int[] dp){
+
+        dp[n - 1] = 0;
+
+        for(int i = n - 2; i >= 0; i--){
+            int min = Integer.MAX_VALUE/2;
+
+            for(int j = 1; j <= nums[i]; j++){
+
+                if(i + j >= n)
+                    break;
+                
+                min = Math.min(min, dp[i + j] + 1);
+            
             }
-            j++;
-            l = r+1;
-            r = f;
+            dp[i] = min;        
         }
-        return j;
+        return dp[0];
+    }
+    
+    public int jump(int[] nums) {
+        int n = nums.length, r = 0, l = 0, jum = 0;
+        int[] dp = new int[n+1];
+
+        // return memo(0, nums, 0, dp);
+        return tabu(n, nums, 0, dp);
     }
 }
