881ace5ed56a665e685dbec67d27ce460e4a9062
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-04 02:03:14 +0530

    Time: 4 ms (72.88%) | Memory: 48.4 MB (22.12%) - LeetSync

--- 621-task-scheduler/task-scheduler.java
+++ 621-task-scheduler/task-scheduler.java
@@ -7,21 +7,17 @@ class Solution {
         Arrays.sort(freq);
 
         int mxFrq = freq[25];
-            System.out.println(tasks.length);
         int totalSlt = mxFrq - 1;
-            System.out.println(totalSlt);
         int idleSlots = totalSlt * n;
-            System.out.println(idleSlots);
 
         for(int i = 24; i >= 0; i--){
-            System.out.println(idleSlots+ " " +freq[i]);
             idleSlots -= Math.min(totalSlt, freq[i]);
-            System.out.println(idleSlots+ " " +freq[i]);
-            if(idleSlots <= 0) return tasks.length;
         }
 
-
-        return tasks.length + idleSlots;
+        if(idleSlots > 0) return tasks.length + idleSlots;
+        
+        
+        return tasks.length;
         
     }
 }
