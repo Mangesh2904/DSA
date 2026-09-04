d8ab17218f45e3a0e3d79b98642c0e00aac7a5e0
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-05 02:07:38 +0530

    Time: 11 ms (5.41%) | Memory: 51.3 MB (88.88%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -10,32 +10,37 @@ class Pair {
 class Solution {
     public int numIslands(char[][] grid) {
         int r = grid.length, c = grid[0].length, ans = 0;
+
         int[][] vis = new int[r][c];
         Queue<Pair> q = new LinkedList<>();
 
-        int[] drow = { -1, 0, 0, 1};
-        int[] dcol = { 0, -1, 1, 0};
+        int drow[] = { -1, 0, 0, 1 };
+        int dcol[] = { 0, 1, -1, 0 };
 
         for (int i = 0; i < r; i++) {
             for (int j = 0; j < c; j++) {
-                if(grid[i][j] == '1' && vis[i][j] == 0) q.offer(new Pair(i, j));
-                vis[i][j] =  1;
-                while (!q.isEmpty()) {
-                    int row = q.peek().r;
-                    int col = q.poll().c;
-                    for (int k = 0; k < 4; k++) {
-                        int newR = row + drow[k];
-                        int newC = col + dcol[k];
 
-                        if (newR >= 0 && newC >= 0 && newR < r && newC < c && vis[newR][newC] != 1 && grid[newR][newC] == '1') {
-                            vis[newR][newC] = 1;
-                            q.offer(new Pair(newR, newC));
-                        }
-                    }
-
-                    if (q.isEmpty())
-                        ans++;
+                if (grid[i][j] == '1' && vis[i][j] != 1) {
+                    q.offer(new Pair(i, j));
+                    ans++;
                 }
+
+                while (!q.isEmpty()) {
+                    int R = q.peek().r;
+                    int C = q.poll().c;
+
+                    for (int k = 0; k < 4; k++) {
+                        int nR = R + drow[k];
+                        int nC = C + dcol[k];
+
+                        if (nR < r && nR >= 0 && nC < c && nC >= 0 && grid[nR][nC] != '0' && vis[nR][nC] != 1) {
+                            vis[nR][nC] = 1;
+                            q.offer(new Pair(nR, nC));
+                        }
+
+                    }
+                }
+
             }
         }
 
