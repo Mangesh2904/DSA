4d3bc5642790daff4d627db349598fe367f9fba5
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-05 07:50:41 +0530

    Time: 1 ms (63.65%) | Memory: 47.5 MB (6.97%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,19 +1,36 @@
 class Solution {
 
-    boolean isB(int src, List<List<Integer>> adj, int[] vis){
+    boolean dfs(int src, int[][] graph, int[] vis) {
 
+        for (int it : graph[src]) {
+            if (vis[it] == 0) {
+                vis[it] = (vis[src] == 1 ? 2 : 1);
+                if (dfs(it, graph, vis) == false)
+                    return false;
+            }
+
+            else if (vis[it] == vis[src])
+                return false;
+        }
+
+        return true;
+    }
+
+    boolean bfs(int src, int[][] graph, int[] vis){
         Queue<Integer> q = new LinkedList<>();
+
         q.offer(src);
         vis[src] = 1;
 
         while(!q.isEmpty()){
             int t = q.poll();
 
-            for(int it : adj.get(t)){
+            for(int it : graph[t]){
                 if(vis[it] == 0){
-                    q.offer(it);
                     vis[it] = (vis[t] == 1 ? 2 : 1);
+                    q.offer(it);
                 }
+
                 else if(vis[it] == vis[t]) return false;
             }
         }
@@ -21,24 +38,22 @@ class Solution {
         return true;
     }
 
-
     public boolean isBipartite(int[][] graph) {
-        List<List<Integer>> adj = new ArrayList<>();
 
-        int[] vis = new int[graph.length];
+        int n = graph.length;
+        int vis[] = new int[n];
 
-        for(int i = 0; i < graph.length; i++) adj.add(new ArrayList<>());
-
-        for(int i = 0; i < graph.length; i++){
-            for(int j : graph[i]) adj.get(i).add(j);
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                // vis[i] = 1;
+                // if (dfs(i, graph, vis) == false)
+                //     return false;
+                if (bfs(i, graph, vis) == false)
+                    return false;
+            }
         }
 
-        for(int i = 0; i < graph.length; i++){
-
-            if(vis[i] == 0)
-                if(isB(i, adj, vis) == false) return false;
-        }
         return true;
-
     }
+
 }
