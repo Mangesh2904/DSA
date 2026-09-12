cfacc94c504034fc1d25b003a65e91c686f45a45
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-13 02:46:48 +0530

    Time: 2 ms (16.54%) | Memory: 49.3 MB (9.76%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,41 +1,42 @@
 class Solution {
 
-    int memo(int idx, int flag, int[] prices, int n, int[][] dp){
-        if(idx == n - 1){
-            if(flag == 1) return prices[idx];
+    int memo(int idx, int flag, int[] prices, int n, int[][] dp) {
+        if (idx == n - 1) {
+            if (flag == 1)
+                return prices[idx];
             return 0;
         }
-        
-        if(dp[idx][flag] != 0) return dp[idx][flag];
 
-        if(flag == 0){
+        if (dp[idx][flag] != 0)
+            return dp[idx][flag];
+
+        if (flag == 0) {
             int buy = -prices[idx] + memo(idx + 1, 1, prices, n, dp);
             int ntBuy = memo(idx + 1, 0, prices, n, dp);
 
             return dp[idx][flag] = Math.max(buy, ntBuy);
         }
-            
-            int sell = prices[idx] + memo(idx + 1, 0, prices, n, dp);
-            int ntSell = memo(idx + 1, 1, prices, n, dp);
 
-            return dp[idx][flag] = Math.max(sell, ntSell); 
+        int sell = prices[idx] + memo(idx + 1, 0, prices, n, dp);
+        int ntSell = memo(idx + 1, 1, prices, n, dp);
+
+        return dp[idx][flag] = Math.max(sell, ntSell);
     }
 
-    int tabu(int[] prices, int n, int[][] dp){
+    int tabu(int[] prices, int n, int[][] dp) {
 
         dp[n - 1][1] = prices[n - 1];
 
-        for(int i = n - 2; i >= 0; i--){
-            for(int j = 0; j < 2; j++){
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j < 2; j++) {
 
-                if(j == 0){
+                if (j == 0) {
                     int buy = -prices[i] + dp[i + 1][j + 1];
                     int ntBuy = dp[i + 1][j];
 
                     dp[i][j] = Math.max(buy, ntBuy);
-                    
-                }
-                else {
+
+                } else {
                     int sell = prices[i] + dp[i + 1][j - 1];
                     int ntSell = dp[i + 1][j];
 
@@ -47,6 +48,37 @@ class Solution {
         return dp[0][0];
     }
 
+    int space(int[] prices, int n) {
+        int[] next = new int[2];
+
+        next[1] = prices[n - 1];
+
+        for (int i = n - 2; i >= 0; i--) {
+            int curr[] = new int[2];
+
+            for (int j = 0; j < 2; j++) {
+
+                if (j == 0) {
+                    int by = -prices[i] + next[1];
+                    int ntBy = next[0];
+
+                    curr[j] = Math.max(by, ntBy);
+
+                } else {
+                    
+                    int sl = prices[i] + next[0];
+                    int ntSl = next[1];
+
+                    curr[j] = Math.max(sl, ntSl);
+                }
+            }
+
+            next = curr;
+        }
+
+        return next[0];
+    }
+
     public int maxProfit(int[] prices) {
 
         int n = prices.length;
@@ -55,7 +87,8 @@ class Solution {
         int[][] dp = new int[n + 1][2];
 
         // return memo(0, 0, prices, n, dp);
-        return tabu(prices, n, dp);
-        
+        // return tabu(prices, n, dp);
+        return space(prices, n);
+
     }
 }
