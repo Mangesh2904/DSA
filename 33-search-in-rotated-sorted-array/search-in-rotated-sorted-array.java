937db3dad33952d1767fd86e2a816b26407e85c2
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-13 17:25:52 +0530

    Time: 0 ms (100.00%) | Memory: 43.9 MB (28.12%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,25 +1,27 @@
 class Solution {
 
+    int bs(int l, int h, int t, int[] nums){
 
-    int bs(int[] nums, int l, int h, int t){
-        if(l > h) return -1 ;
-        
         int m = (l + h) / 2;
+
+        if(l > h) return -1;
+
         if(nums[m] == t) return m;
 
-        if(nums[m] >= nums[l]){
-            if(nums[l] <= t && nums[m] >= t) return bs(nums, l, m-1, t);
-            else return bs(nums, m+1, h, t);
+        if(nums[m] <= nums[h]){
+            if(t > nums[m] && t <= nums[h]) return bs(m + 1, h, t, nums);
+            else return bs(l, m - 1, t, nums);
         }
-        else{
-            if(nums[h] >= t && nums[m] <= t) return bs(nums, m+1, h, t);
-            else return bs(nums, l, m-1, t);
-        }
-        
-    }
 
+        else {
+            if(t >= nums[l] && t < nums[m]) return bs(l, m - 1, t, nums);
+            else return bs(m + 1, h, t, nums);
+        }
+    }
     
     public int search(int[] nums, int target) {
-        return bs(nums, 0, nums.length - 1, target);
+        int l = 0, h = nums.length - 1;
+
+        return bs(l, h, target, nums);
     }
 }
