2467270cdc5e353ba80ca6b2d0182369a7e3a43b
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-14 00:24:07 +0530

    Time: 1631 ms (5.00%) | Memory: 66.5 MB (5.09%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,4 +1,47 @@
 class Solution {
+
+    List<List<Integer>> sum3(int[] nums, int n){
+
+        List<List<Integer>> ans = new ArrayList<>();
+        
+        Arrays.sort(nums);
+
+        int j, k;
+
+        for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
+            j = i + 1;
+            k = n - 1;
+
+            while(j < k){
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if(sum < 0) j++;
+
+                else if(sum > 0) k--;
+
+                else{
+                    List<Integer> list = new ArrayList<>();
+                    
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(nums[k]);
+
+                    ans.add(list);
+
+                    j++;
+                    k--;
+
+                    while(j < k && nums[j] == nums[j - 1]) j++;
+                    while(j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+    
     public List<List<Integer>> threeSum(int[] nums) {
         HashSet<List<Integer>> st = new HashSet<>();
 
@@ -31,7 +74,10 @@ class Solution {
 
         for(List<Integer> ls : st) ans.add(ls);
 
-        return ans;
+        // return ans;
+
+
+        return sum3(nums, n);
 
     }
 }
