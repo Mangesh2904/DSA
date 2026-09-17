5c339199afd514234818acc7fb89a591c6ac822e
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-17 23:48:04 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (20.17%) - LeetSync

--- 162-find-peak-element/find-peak-element.java
+++ 162-find-peak-element/find-peak-element.java
@@ -1,19 +1,25 @@
 class Solution {
 
-    int bs(int[] nums, int l, int h){
-        if(l > h) return -1;
+    int bs(int l, int r, int[] nums){
+        int n = nums.length;
 
-        int m = (l+h)/2;
+        int m = (l + r) / 2;
 
-        if(nums[m] > nums[m-1] && nums[m] > nums[m+1]) return m;
-        else if(nums[m] > nums[m-1]) return bs(nums, m+1, h);
-        else return bs(nums, l, m-1);
+        if(nums[m] > nums[m - 1] && nums[m] > nums[m + 1]) return m;
+
+        if(nums[m] > nums[m - 1]) return bs(m + 1, r, nums);
+
+        return bs(l, m - 1, nums);
     }
+
 
     public int findPeakElement(int[] nums) {
         int n = nums.length;
-        if((n == 1) || (nums[0] > nums[1])) return 0;
-        if(nums[n-1] > nums[n-2]) return n-1;
-        return bs(nums, 1, n-2);
+
+        if(n == 1 || nums[0] > nums[1]) return 0;
+
+        if(nums[n - 1] > nums[n - 2]) return n - 1;
+
+        return bs(1, n - 2, nums);
     }
 }
