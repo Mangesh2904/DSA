e7bf1e6f728f8f3c4e2f5d0f177edb801444d809
Mangesh_Shah <168351103+Mangesh2904@users.noreply.github.com>
2026-09-20 02:01:36 +0530

    Time: 1 ms (56.04%) | Memory: 48.6 MB (20.85%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.java
+++ 42-trapping-rain-water/trapping-rain-water.java
@@ -1,45 +1,51 @@
 class Solution {
-    public int[] rightMax(int[] arr){
-        int[] rm = new int[arr.length];
-        int k = arr[arr.length - 1];
-        rm[arr.length - 1] = -1;
-        for(int i = arr.length - 2; i >= 0 ; i--){
-            if(k > arr[i]) rm[i] = k;
 
-            else{
-                k = arr[i];
-                rm[i] = -1;
-            }
-        }
-        
-        return rm;
+    int[] maxRight(int n, int nums[]){
+        int ans[] = new int[n];
 
+        Arrays.fill(ans, -1);
+
+        int mx = nums[n - 1];
+
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] > mx) mx = nums[i];
+
+            else  ans[i] = mx;
+        }   
+
+        return ans;
     }
-    public int[] leftMax(int[] arr){
-        int[] lm = new int[arr.length];
-        int k = arr[0];
-        lm[0] = -1;
-        for(int i = 1; i < arr.length; i++){
-            if(k > arr[i]) lm[i] = k;
 
-            else{
-                k = arr[i];
-                lm[i] = -1;
-            }
-        }
-        
-        return lm;
+    int[] maxLeft(int n, int nums[]){
+        int ans[] = new int[n];
 
+        Arrays.fill(ans, -1);
+
+        int mx = nums[0];
+
+        for(int i = 1; i < n; i++){
+            if(nums[i] > mx) mx = nums[i];
+
+            else ans[i] = mx;  
+        }   
+
+        return ans;
     }
+    
     public int trap(int[] height) {
-        int[] lm = leftMax(height);
-        int[] rm = rightMax(height);
-        int res = 0;
-        for(int i = 1; i < height.length - 1; i++){
-            if(height[i] < lm[i] && height[i] < rm[i]) 
-                res += Math.min(lm[i] , rm[i]) - height[i];
+
+        int n = height.length, ans = 0;
+
+        int[] leftMx = maxLeft(n, height);
+        int[] rightMx = maxRight(n, height);
+
+        for(int i = 0; i < n; i++){
+            if(leftMx[i] == -1 || rightMx[i] == -1) continue;
+            
+            ans += Math.min(leftMx[i], rightMx[i]) - height[i];
         }
 
-        return res;
+        return ans;
+        
     }
 }
